5a63277f5dcacdae8c9c40590b0529fea503c982
renessanssi <tuomas.t.suni@gmail.com>
2025-10-18 12:21:37 +0300

    QoL updates
    - Icon
    - Pop Preset

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ resetBtn.addEventListener('click', () => {
 const presets = {
   boostBass: { bass: 19, mid: -30, treble: -30 },
   boostMetal: { bass: 30, mid: 0, treble: 0 },
+  boostPop: { bass: 0, mid: 11, treble: 30 },
   cancelNoise: { bass: 0, mid: -30, treble: -30 }
 };
 
